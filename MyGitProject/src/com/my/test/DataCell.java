65338948e144a2ea269432fd96e095d10d5a602b
Debajit Saikia <dsaikia@gmail.com>
2014-01-13 18:19:25 +0530

    Initial check in

---
+++ MyGitProject/src/com/my/test/DataCell.java
@@ -0,0 +1,10 @@
+package com.my.test;
+
+public class DataCell {
+	
+	public void display()
+	{
+		System.out.println("inside display"); 
+	}
+
+}
---
+++ MyGitProject/src/com/my/test/Runner.java
@@ -0,0 +1,20 @@
+package com.my.test;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		DataCell dc=new DataCell();
+		dc.display();
+		test();	
+		
+	}
+
+	private static void test() 
+	{
+		System.out.println("Hi test");
+		
+	}
+
+}
